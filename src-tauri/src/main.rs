d7663d49c7dc4875036ca8a3aba58cbb24be6144
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-12-16 01:13:37 +0100

    Testing "updater"

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,6 +11,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .setup(|app| {
             #[cfg(target_os = "windows")]
