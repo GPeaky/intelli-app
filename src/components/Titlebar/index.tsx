1a36eb40f6f60645539faf7e57b5b7d4447bf2d9
Gerard Zinn <gerardjoven2020@hotmail.com>
2023-12-21 22:32:02 +0100

    Changed to Tailwindcss

--- src/components/Titlebar/index.tsx
+++ src/components/Titlebar/index.tsx
@@ -1,20 +1,19 @@
-import './index.css'
-import { IconMinus, IconSquare, IconX } from '@tabler/icons-solidjs'
 import { getCurrent } from '@tauri-apps/api/window'
+import { IconMinus, IconSquare, IconX } from '@tabler/icons-solidjs'
 
 const TitleBar = () => {
     const appWindow = getCurrent()
 
     return (
-        <div data-tauri-drag-region class="titlebar">
-            <div class="titlebar-button" onClick={() => appWindow.minimize()} >
-                <IconMinus size={14} />
+        <div data-tauri-drag-region class="h-5 bg-neutral-800 select-none flex justify-end fixed inset-0">
+            <div class="inline-flex justify-center items-center p-1 hover:bg-neutral-700" onClick={() => appWindow.minimize()} >
+                <IconMinus class='text-neutral-400' size={14} />
             </div>
-            <div class="titlebar-button" onClick={() => appWindow.toggleMaximize()}>
-                <IconSquare size={12} />
+            <div class="inline-flex justify-center items-center p-1 hover:bg-neutral-700" onClick={() => appWindow.toggleMaximize()}>
+                <IconSquare class='text-neutral-400' size={12} />
             </div>
-            <div class="titlebar-button" onClick={() => appWindow.close()}>
-                <IconX size={14} />
+            <div class="inline-flex justify-center items-center p-1 hover:bg-red-600" onClick={() => appWindow.close()}>
+                <IconX class='text-neutral-400' size={14} />
             </div>
         </div>
     )
